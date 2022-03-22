8f984ef43632617a451fa7626f570c93379b9f3b
yejinchoi115 <yjjjang115@gmail.com>
2022-03-21 20:52:54 -0600

    yejin - 1st commit

---
+++ TempleScheduler/Models/AppointmentContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TempleScheduler.Models
+{
+    public class AppointmentContext: DbContext
+    {
+        public AppointmentContext(DbContextOptions<AppointmentContext> options) : base(options)
+        {
+
+        }
+        public DbSet<AppointmentContext> Appointments { get; set; }
+    }
+}
--- TempleScheduler/Models/Group.cs
+++ TempleScheduler/Models/Group.cs
@@ -10,17 +10,22 @@ namespace TempleScheduler.Models
     {
         [Required]
         [Key]
-        public int GroupId { get; set; }
+        public int AppointmentId { get; set; }
 
-        [Required]
-        public string Name { get; set; }
+        [Required(ErrorMessage = "Enter the Group Name")]
+        public string GroupName { get; set; }
 
-        [Range(1,15)]
-        public string Size { get; set; }
+        [Range(1,15, ErrorMessage = "Group size should be in 1 -15")]
+        public string GroupSize { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Enter the Email Address")]
         public string Email { get; set; }
 
-        public string phone { get; set; }
+        // optional
+        public string Phone { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
     }
 }
