b0f6c4f27af564913c69c92d34cacba63e9c9b96
yejinchoi115 <yjjjang115@gmail.com>
2022-03-21 21:20:52 -0600

    yejin-2nd commit

--- TempleScheduler/Controllers/HomeController.cs
+++ TempleScheduler/Controllers/HomeController.cs
@@ -28,6 +28,7 @@ namespace TempleScheduler.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
